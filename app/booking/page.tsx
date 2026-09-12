6acc993551134033ec68e66d5404085330fcb769
Ak David <120152054+akdavid4real@users.noreply.github.com>
2026-09-12 13:38:34 +0100

    Wire booking page to dedicated client submit handler

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,5 +1,11 @@
 import { LegacyPage } from '@/components/LegacyPage'
+import { BookingClient } from '@/components/BookingClient'
 
 export default function Page() {
-  return <LegacyPage fileName="booking.html" />
+  return (
+    <>
+      <LegacyPage fileName="booking.html" />
+      <BookingClient />
+    </>
+  )
 }
